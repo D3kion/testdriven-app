6228ab5ccce543e37b9595baece59261e88bf32d
Roman Salikov <dekion@protonmail.com>
2018-12-24 03:55:58 +0300

    Fix

--- services/users/project/tests/test_user_model.py
+++ services/users/project/tests/test_user_model.py
@@ -39,13 +39,13 @@ class TestUserModel(BaseTestCase):
         user = add_user('justatest', 'test@test.com', 'test')
         auth_token = user.encode_auth_token(user.id)
         self.assertTrue(isinstance(auth_token, bytes))
-    
+
     def test_decode_auth_token(self):
         user = add_user('justatest', 'test@test.com', 'test')
         auth_token = user.encode_auth_token(user.id)
         self.assertTrue(isinstance(auth_token, bytes))
         self.assertEqual(User.decode_auth_token(auth_token), user.id)
-        
+
 
 if __name__ == "__main__":
     unittest.main()
